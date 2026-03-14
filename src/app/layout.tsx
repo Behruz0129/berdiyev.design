c84c51d6905f6663c4f0517745bc2da513b065ce
Behruz <behruz.berdiyev.0129@gmail.com>
2026-03-14 05:19:22 +0500

    analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { cookies } from "next/headers";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Providers } from "./providers";
 import { LocaleProvider } from "@/contexts/LocaleContext";
@@ -50,6 +51,7 @@ export default async function RootLayout({
             </div>
           </LocaleProvider>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
